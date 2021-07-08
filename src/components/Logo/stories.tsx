c5e6597f1dc547194fd484d2554ea83721c940b2
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-08 14:42:43 -0300

    Creating a Logo component and adding a logo svg Optimized with SVGOMG

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,13 @@
+import { Story, Meta } from '@storybook/react'
+import Logo from '.'
+
+export default {
+  title: 'Logo',
+  component: Logo,
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Logo {...args} />
