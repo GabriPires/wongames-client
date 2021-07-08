ccff2ab813204b717e04e7df9a1eb23e192e1b21
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-08 15:39:49 -0300

    Creating firsts tests in logo colors

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
@@ -7,5 +8,17 @@ describe('<Logo />', () => {
     // renderizar o componente `render`
     // selecionar o elemento a ser testado `screen` (queries)
     // expect - assertion - asserção - comparação - análise (o que é esperado)
+
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
