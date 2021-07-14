9c1eb9b2b53f4eccd0aee674be15cd14b7e110b6
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-14 17:37:36 -0300

    Creating sign in form component Creating tests for component

---
+++ src/components/FormSignIn/stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from '@storybook/react'
+import FormSignIn from '.'
+
+export default {
+  title: 'Form/FormSignIn',
+  component: FormSignIn
+  // args: {}
+} as Meta
+
+export const Default: Story = (args) => (
+  <div style={{ width: 300, margin: 'auto' }}>
+    <FormSignIn {...args} />
+  </div>
+)
---
+++ src/components/FormSignIn/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main``
+
+export const FogotPassword = styled.a``
+
+export const FormLink = styled.div``
