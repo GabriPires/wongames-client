ca27c9a6c9e31d4ba14eb4d2a61294f760539b2d
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-14 14:05:50 -0300

    Add onChange method for checkbox

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -4,7 +4,9 @@ import Checkbox, { CheckboxProps } from '.'
 export default {
   title: 'Checkbox',
   component: Checkbox,
-  args: {}
+  argTypes: {
+    onCheck: { action: 'checked' }
+  }
 } as Meta
 
 export const Default: Story<CheckboxProps> = (args) => <Checkbox {...args} />
