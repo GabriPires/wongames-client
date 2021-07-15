76be7dbfa6e762689eedd3bd0516a7de0b88c62a
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-15 12:35:29 -0300

    Styling gameinfo component and build a snapshot

--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -14,7 +14,7 @@ export default {
 } as Meta
 
 export const Default: Story<GameInfoProps> = (args) => (
-  <div style={{ maxWidth: '144rem', padding: '1.rem' }}>
+  <div style={{ maxWidth: '144rem', margin: 'auto', padding: '1.rem' }}>
     <GameInfo {...args} />
   </div>
 )
