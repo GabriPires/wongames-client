c635b3f732c293395048d3eda5775b58612403b1
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-16 10:45:34 -0300

    Creating tests to game template Fixing mocks types

--- src/components/GameDetails/mock.ts
+++ src/components/GameDetails/mock.ts
@@ -2,7 +2,7 @@ export default {
   developer: 'Different Tales',
   releaseDate: '2020-11-21T23:00:00',
   platforms: ['windows', 'mac', 'linux'],
-  publishers: 'Walkabout',
+  publisher: 'Walkabout',
   rating: 'BR0',
   genres: ['Role-playing']
 }
