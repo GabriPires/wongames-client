819436752172203f114c9be1e474e25f63ed0393
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-16 14:38:44 -0300

    New component cart list

---
+++ src/components/CartList/stories.tsx
@@ -0,0 +1,29 @@
+import { Story, Meta } from '@storybook/react'
+import CartList, { CartListProps } from '.'
+
+import mockItems from './mock'
+
+export default {
+  title: 'CartList',
+  component: CartList,
+  argTypes: {
+    items: {
+      type: ''
+    }
+  },
+  args: {
+    items: mockItems,
+    total: 'R$ 330,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<CartListProps> = (args) => (
+  <div style={{ maxWidth: '80rem', margin: '0 auto' }}>
+    <CartList {...args} />
+  </div>
+)
