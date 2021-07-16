7833213c4ff8c65361172d38ec8df7e8fb171b63
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-16 16:35:43 -0300

    Creating profile menu component and testing render

---
+++ src/components/ProfileMenu/stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react'
+import ProfileMenu from '.'
+
+export default {
+  title: 'ProfileMenu',
+  component: ProfileMenu,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+  // args: {}
+} as Meta
+
+export const Default: Story = (args) => <ProfileMenu {...args} />
