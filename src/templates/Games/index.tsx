4e9acd4353dcdf249fc5cfaae032e9cff1cc40ad
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-19 11:52:00 -0300

    Building games page with components Adjust to responsive Testing

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,16 +1,46 @@
-import { GameCardProps } from 'components/GameCard'
+import Base from 'templates/Base'
+import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined'
+
+import { Grid } from 'components/Grid'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import ExploreSidebar, { ItemProps } from 'components/ExploreSideBar'
 
 import * as S from './styles'
 
 export type GamesTemplateProps = {
   games?: GameCardProps[]
+  filterItems: ItemProps[]
 }
 
-const GamesTemplate = ({ games = [] }: GamesTemplateProps) => (
-  <S.Wrapper>
-    <h1>GamesTemplate</h1>
-    <p>{games}</p>
-  </S.Wrapper>
-)
+const GamesTemplate = ({ games = [], filterItems }: GamesTemplateProps) => {
+  const handleFilter = () => {
+    return
+  }
+
+  const handleShowMore = () => {
+    return
+  }
+
+  return (
+    <Base>
+      <S.Main>
+        <ExploreSidebar items={filterItems} onFilter={handleFilter} />
+
+        <section>
+          <Grid>
+            {games.map((game) => (
+              <GameCard key={game.title} {...game} />
+            ))}
+          </Grid>
+
+          <S.ShowMore role="button" onClick={handleShowMore}>
+            <p>Show more</p>
+            <ArrowDown size={35} />
+          </S.ShowMore>
+        </section>
+      </S.Main>
+    </Base>
+  )
+}
 
 export default GamesTemplate
