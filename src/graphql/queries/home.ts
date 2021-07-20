37acfdae63cf330c74e2473f1ff3639b6a693602
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-20 12:07:28 -0300

    Popular games data from api and new types

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -1,6 +1,7 @@
 import { gql } from '@apollo/client'
 import { BannerFragment } from 'graphql/fragments/banner'
 import { GameFragment } from 'graphql/fragments/game'
+import { HighlightFrament } from 'graphql/fragments/highlight'
 
 export const QUERY_HOME = gql`
   query QueryHome {
@@ -27,8 +28,42 @@ export const QUERY_HOME = gql`
     freeGames: games(where: { price: 0 }, sort: "release_date:desc", limit: 8) {
       ...GameFragment
     }
+
+    sections: home {
+      newGames {
+        title
+        highlight {
+          ...HighlightFrament
+        }
+      }
+
+      popularGames {
+        title
+        highlight {
+          ...HighlightFrament
+        }
+        games(limit: 8) {
+          ...GameFragment
+        }
+      }
+
+      upcomingGames {
+        title
+        highlight {
+          ...HighlightFrament
+        }
+      }
+
+      freeGames {
+        title
+        highlight {
+          ...HighlightFrament
+        }
+      }
+    }
   }
 
   ${BannerFragment}
   ${GameFragment}
+  ${HighlightFrament}
 `
