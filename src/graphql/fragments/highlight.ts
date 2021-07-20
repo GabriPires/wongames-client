d0daa765b5dd491166a20a3e2db2b95f73935609
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-20 14:06:42 -0300

    Fixing types with wrong name

--- src/graphql/fragments/highlight.ts
+++ src/graphql/fragments/highlight.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 
-export const HighlightFrament = gql`
-  fragment HighlightFrament on ComponentPageHighlight {
+export const HighlightFragment = gql`
+  fragment HighlightFragment on ComponentPageHighlight {
     title
     subtitle
     background {
