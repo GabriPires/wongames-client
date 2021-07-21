5a8df790f1c5b5b660f02b386746a92f353f6d15
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-21 16:24:45 -0300

    Fixing cartMapper test in wrong file

--- src/hooks/use-cart/test.tsx
+++ src/hooks/use-cart/test.tsx
@@ -1,8 +1,6 @@
 import { MockedProvider } from '@apollo/client/testing'
 import { renderHook } from '@testing-library/react-hooks'
-import { QueryGames_games } from 'graphql/generated/QueryGames'
 import { setStorageItem } from 'utils/localStorage'
-import { cartMapper } from 'utils/mappers'
 
 import { useCart, CartProvider, CartProviderProps } from '.'
 import { cartItems, gamesMock } from './mock'
@@ -24,31 +22,5 @@ describe('useCart', () => {
     await waitForNextUpdate()
 
     expect(result.current.items).toStrictEqual(cartItems)
-  })
-})
-
-describe('cartMapper()', () => {
-  it('should return empty array if no games', () => {
-    expect(cartMapper(undefined)).toStrictEqual([])
-  })
-
-  it('should return mapper items', () => {
-    const game = {
-      id: '1',
-      cover: {
-        url: '/image.jpg'
-      },
-      name: 'game',
-      price: 10
-    } as QueryGames_games
-
-    expect(cartMapper([game])).toStrictEqual([
-      {
-        id: '1',
-        img: 'http://localhost:1337/image.jpg',
-        title: 'game',
-        price: '$10.00'
-      }
-    ])
   })
 })
