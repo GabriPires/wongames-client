26138a60d26100a115ad7941c7defcf87919a8f9
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-21 16:14:42 -0300

    Creating cartMapper and  searching data in api

--- src/hooks/use-cart/test.tsx
+++ src/hooks/use-cart/test.tsx
@@ -1,18 +1,54 @@
+import { MockedProvider } from '@apollo/client/testing'
 import { renderHook } from '@testing-library/react-hooks'
+import { QueryGames_games } from 'graphql/generated/QueryGames'
 import { setStorageItem } from 'utils/localStorage'
+import { cartMapper } from 'utils/mappers'
 
 import { useCart, CartProvider, CartProviderProps } from '.'
+import { cartItems, gamesMock } from './mock'
 
 describe('useCart', () => {
-  it('should return item and its info if there are any in the cart', () => {
+  it('should return item and its info if there are any in the cart', async () => {
     const wrapper = ({ children }: CartProviderProps) => (
-      <CartProvider>{children}</CartProvider>
+      <MockedProvider mocks={[gamesMock]}>
+        <CartProvider>{children}</CartProvider>
+      </MockedProvider>
     )
 
     setStorageItem('cartItems', ['1', '2'])
 
-    const { result } = renderHook(() => useCart(), { wrapper })
+    const { result, waitForNextUpdate } = renderHook(() => useCart(), {
+      wrapper
+    })
 
-    expect(result.current.items).toStrictEqual(['1', '2'])
+    await waitForNextUpdate()
+
+    expect(result.current.items).toStrictEqual(cartItems)
+  })
+})
+
+describe('cartMapper()', () => {
+  it('should return empty array if no games', () => {
+    expect(cartMapper(undefined)).toStrictEqual([])
+  })
+
+  it('should return mapper items', () => {
+    const game = {
+      id: '1',
+      cover: {
+        url: '/image.jpg'
+      },
+      name: 'game',
+      price: 10
+    } as QueryGames_games
+
+    expect(cartMapper([game])).toStrictEqual([
+      {
+        id: '1',
+        img: 'http://localhost:1337/image.jpg',
+        title: 'game',
+        price: '$10.00'
+      }
+    ])
   })
 })
