aeb237e6d22f34e821160d1440f1eb4d33543c2e
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-21 16:52:04 -0300

    Add intermediary method to save to cart

--- src/hooks/use-cart/index.tsx
+++ src/hooks/use-cart/index.tsx
@@ -65,16 +65,18 @@ const CartProvider = ({ children }: CartProviderProps) => {
 
   const isInCart = (id: string) => (id ? cartItems.includes(id) : false)
 
+  const saveCart = (cartItems: string[]) => {
+    setCartItems(cartItems)
+    setStorageItem(CART_KEY, cartItems)
+  }
+
   const addToCart = (id: string) => {
-    const newCartItems = [...cartItems, id]
-    setCartItems(newCartItems)
-    setStorageItem(CART_KEY, newCartItems)
+    saveCart([...cartItems, id])
   }
 
   const removeFromCart = (id: string) => {
     const newCartItems = cartItems.filter((itemId: string) => itemId !== id)
-    setCartItems(newCartItems)
-    setStorageItem(CART_KEY, newCartItems)
+    saveCart(newCartItems)
   }
 
   return (
