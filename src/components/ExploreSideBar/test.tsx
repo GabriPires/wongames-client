3a662154fefc456ba2cb1b581839987485dfd08c
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-21 10:08:10 -0300

    Removing filter button on desktop screen Testing new function of button

--- src/components/ExploreSideBar/test.tsx
+++ src/components/ExploreSideBar/test.tsx
@@ -75,8 +75,6 @@ describe('<ExploreSideBar />', () => {
       />
     )
 
-    userEvent.click(screen.getByRole('button', { name: /filter/i }))
-
     expect(onFilter).toBeCalledWith({
       platforms: ['windows'],
       sort_by: 'low-to-high'
@@ -91,7 +89,7 @@ describe('<ExploreSideBar />', () => {
     userEvent.click(screen.getByLabelText(/linux/i))
     userEvent.click(screen.getByLabelText(/low to high/i))
 
-    userEvent.click(screen.getByRole('button', { name: /filter/i }))
+    expect(onFilter).toHaveBeenCalledTimes(4)
 
     expect(onFilter).toBeCalledWith({
       platforms: ['windows', 'linux'],
@@ -105,8 +103,6 @@ describe('<ExploreSideBar />', () => {
 
     userEvent.click(screen.getByLabelText(/low to high/i))
     userEvent.click(screen.getByLabelText(/high to low/i))
-
-    userEvent.click(screen.getByRole('button', { name: /filter/i }))
 
     expect(onFilter).toBeCalledWith({ sort_by: 'high-to-low' })
   })
@@ -133,6 +129,12 @@ it('should open/close sidebar when filtering on mobile ', () => {
   expect(Element).toHaveStyleRule('opacity', '1', variant)
 
   userEvent.click(screen.getByLabelText(/close filters/))
+
+  expect(Element).not.toHaveStyleRule('opacity', '1', variant)
+
+  userEvent.click(screen.getByLabelText(/open filters/))
+
+  userEvent.click(screen.getByRole('button', { name: /filter/i }))
 
   expect(Element).not.toHaveStyleRule('opacity', '1', variant)
 })
