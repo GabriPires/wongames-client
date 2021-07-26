57bb144864c1551a45cd3dec0dfc3a669f930ebf
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-26 12:12:44 -0300

    Cart page with protection Fix break redirect on windows

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,18 +1,21 @@
 import Cart, { CartProps } from 'templates/Cart'
+import { GetServerSidePropsContext } from 'next'
 
 import itemsMock from 'components/CartList/mock'
 import cardsMock from 'components/PaymentOptions/mock'
 import { initializeApollo } from 'utils/apollo'
 import { QueryRecommended } from 'graphql/generated/QueryRecommended'
 import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import protectedRoutes from 'utils/protected-routes'
 import { gamesMapper, highlightMapper } from 'utils/mappers/index'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
-export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
 
   const {
     data: { recommended }
@@ -21,6 +24,7 @@ export async function getServerSideProps() {
   })
   return {
     props: {
+      session,
       items: itemsMock,
       total: 'R$ 430,00',
       cards: cardsMock,
