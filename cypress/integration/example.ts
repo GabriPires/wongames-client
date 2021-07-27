4160f92ecff3519c95ec5fce7500355a8330fb3e
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-27 14:01:30 -0300

    Fists tests with cypress

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -1,7 +1,22 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Cypress TS', () => {
-  it('should go to google', () => {
+  it.skip('should go to google', () => {
     cy.google()
+  })
+
+  it.skip('should change light/dark theme on willian justen site', () => {
+    cy.visit('https://willianjusten.com.br')
+
+    cy.findByTitle(/mudar o tema/i).click()
+    cy.get('.light').should('exist')
+
+    cy.findByTitle(/mudar o tema/i).click()
+    cy.get('.dark').should('exist')
+  })
+
+  it('should visit won games', () => {
+    cy.visit('http://localhost:3000')
+    cy.findByText(/townscaper/i).should('exist')
   })
 })
