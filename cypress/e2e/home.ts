7bbc6e809255b8d7f79b67a8606a6e9cc3c44965
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-27 14:48:55 -0300

    Testing banner

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -5,9 +5,16 @@ describe('Home Page', () => {
     // visitar a página
     cy.visit('/')
 
+    // selecionou os banners
     cy.get('.slick-slider').within(() => {
       cy.findByRole('heading', { name: /townscaper/i })
       cy.findByRole('link', { name: /build your own city/i })
+
+      cy.get('.slick-dots > :nth-child(2) > button').click()
+      cy.wait(700)
+
+      cy.findByRole('heading', { name: /cyberpunk 2077/i })
+      cy.findByRole('link', { name: /buy now/i })
     })
   })
 })
