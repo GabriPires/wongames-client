4b241ed6c5d38d38cf423ed7ea75f53cee7c6942
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-27 11:01:57 -0300

    Optmizing Highlight component with next/image

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -1,5 +1,4 @@
-import { screen } from '@testing-library/react'
-import { renderWithTheme } from 'utils/tests/helpers'
+import { screen, render } from 'utils/test-utils'
 
 import Highlight from '.'
 import * as S from './styles'
@@ -14,7 +13,7 @@ const props = {
 
 describe('<Highlight />', () => {
   it('should render headings and button', () => {
-    renderWithTheme(<Highlight {...props} />)
+    render(<Highlight {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /heading 1/i })
@@ -28,24 +27,19 @@ describe('<Highlight />', () => {
   })
 
   it('should render background imamge', () => {
-    const { container } = renderWithTheme(<Highlight {...props} />)
+    render(<Highlight {...props} />)
 
-    expect(container.firstChild).toHaveStyle({
-      backgroundImage: `url(${props.backgroundImage})`
-    })
+    expect(screen.getByRole('img', { name: props.title })).toBeInTheDocument()
   })
 
   it('should render float imamge', () => {
-    renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />)
+    render(<Highlight {...props} floatImage="/float-image.png" />)
 
-    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
-      'src',
-      '/float-image.png'
-    )
+    expect(screen.getAllByRole('img', { name: props.title })).toHaveLength(2)
   })
 
   it('should render align right by default', () => {
-    const { container } = renderWithTheme(<Highlight {...props} />)
+    const { container } = render(<Highlight {...props} />)
 
     expect(container.firstChild).toHaveStyleRule(
       'grid-template-areas',
@@ -58,9 +52,7 @@ describe('<Highlight />', () => {
   })
 
   it('should render align left', () => {
-    const { container } = renderWithTheme(
-      <Highlight {...props} alignment="left" />
-    )
+    const { container } = render(<Highlight {...props} alignment="left" />)
 
     expect(container.firstChild).toHaveStyleRule(
       'grid-template-areas',
