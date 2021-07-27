c270cfa38dbcd3b638ae2346286b01d0dfc91a41
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-27 14:54:40 -0300

    Creating customized command for render banner

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -27,3 +27,16 @@
 import '@testing-library/cypress/add-commands'
 
 Cypress.Commands.add('google', () => cy.visit('https://google.com'))
+
+Cypress.Commands.add('shouldRenderBanner', () => {
+  cy.get('.slick-slider').within(() => {
+    cy.findByRole('heading', { name: /townscaper/i })
+    cy.findByRole('link', { name: /build your own city/i })
+
+    cy.get('.slick-dots > :nth-child(2) > button').click()
+    cy.wait(700)
+
+    cy.findByRole('heading', { name: /cyberpunk 2077/i })
+    cy.findByRole('link', { name: /buy now/i })
+  })
+})
