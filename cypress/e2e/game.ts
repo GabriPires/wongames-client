c62e280519fc4252492c737c352c975585ef9630
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-27 16:14:18 -0300

    Testing game page add/remove games in cart

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -1,14 +1,17 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Game Page', () => {
-  it('should render game page sections', () => {
-    cy.visit('/game/cyberpunk-2077')
-    cy.wait(3000)
+  before(() => {
+    cy.visit('/game/ys-ix-monstrum-nox-digital-deluxe-edition')
+  })
 
+  it('should render game page sections', () => {
     cy.getByDataCy('game-info').within(() => {
-      cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist')
-      cy.findByText(/^Cyberpunk 2077 is an open-world/i).should('exist')
-      cy.findByText('$133.99').should('exist')
+      cy.findByRole('heading', {
+        name: /Ys IX: Monstrum Nox Digital Deluxe Edition/i
+      }).should('exist')
+      cy.findByText(/^free DLC for Limited time only!/i).should('exist')
+      cy.findByText('$401.19').should('exist')
       cy.findByRole('button', { name: /add to cart/i }).should('exist')
     })
 
@@ -35,11 +38,12 @@ describe('Game Page', () => {
       cy.findByRole('heading', { name: /rating/i }).should('exist')
       cy.findByRole('heading', { name: /genres/i }).should('exist')
 
-      cy.findAllByText(/cd projekt red/i).should('have.length', 2)
-      cy.findByText(/dec 8, 2020/i).should('exist')
+      cy.findByText(/Nihon Falcom Corporation/i).should('exist')
+      cy.findByText(/NIS America, Inc./i).should('exist')
+      cy.findByText(/Jul 4, 2021/i).should('exist')
       cy.findByRole('img', { name: /windows/i }).should('exist')
       cy.findByText(/free/i).should('exist')
-      cy.findByText('Action / Role-playing / Sci-fi').should('exist')
+      cy.findByText('Action / Role-playing / JRPG').should('exist')
     })
 
     cy.shouldRenderShowcase({ name: 'Upcoming', highlight: true })
@@ -47,5 +51,37 @@ describe('Game Page', () => {
       name: 'You may like these games',
       highlight: false
     })
+  })
+
+  it('should add/remove game in cart', () => {
+    // add to cart
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('button', { name: /add to cart/i }).click()
+      cy.findByRole('button', { name: /remove from cart/i }).should('exist')
+    })
+
+    cy.findAllByLabelText(/Cart Items/i)
+      .first()
+      .should('have.text', 1)
+      .click()
+
+    cy.getByDataCy('cart-list').within(() => {
+      cy.findByRole('heading', {
+        name: /Ys IX: Monstrum Nox Digital Deluxe Edition/i
+      }).should('exist')
+    })
+
+    // close dropdown
+    cy.findAllByLabelText(/Cart Items/i)
+      .first()
+      .click()
+
+    // remove from cart
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('button', { name: /remove from cart/i }).click()
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
+    })
+
+    cy.findAllByLabelText(/Cart Items/i).should('not.exist')
   })
 })
