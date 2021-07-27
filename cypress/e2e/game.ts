3091cd6f4247c98488161de4741006cf9c2e0b8e
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-27 15:53:39 -0300

    Testing game details and showcases

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -3,7 +3,7 @@
 describe('Game Page', () => {
   it('should render game page sections', () => {
     cy.visit('/game/cyberpunk-2077')
-    cy.wait(2000)
+    cy.wait(3000)
 
     cy.getByDataCy('game-info').within(() => {
       cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist')
@@ -24,5 +24,28 @@ describe('Game Page', () => {
     })
 
     cy.getByDataCy('content').children().should('have.length.at.least', 2)
+
+    // game details
+    cy.getByDataCy('game-details').within(() => {
+      cy.findByRole('heading', { name: /game details/i }).should('exist')
+      cy.findByRole('heading', { name: /developer/i }).should('exist')
+      cy.findByRole('heading', { name: /release date/i }).should('exist')
+      cy.findByRole('heading', { name: /platforms/i }).should('exist')
+      cy.findByRole('heading', { name: /publisher/i }).should('exist')
+      cy.findByRole('heading', { name: /rating/i }).should('exist')
+      cy.findByRole('heading', { name: /genres/i }).should('exist')
+
+      cy.findAllByText(/cd projekt red/i).should('have.length', 2)
+      cy.findByText(/dec 8, 2020/i).should('exist')
+      cy.findByRole('img', { name: /windows/i }).should('exist')
+      cy.findByText(/free/i).should('exist')
+      cy.findByText('Action / Role-playing / Sci-fi').should('exist')
+    })
+
+    cy.shouldRenderShowcase({ name: 'Upcoming', highlight: true })
+    cy.shouldRenderShowcase({
+      name: 'You may like these games',
+      highlight: false
+    })
   })
 })
