7447da1189a139f0cb5cd1958aae5b3fd7997b49
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-28 08:36:39 -0300

    Testing wrong paths for reset-password

---
+++ cypress/e2e/reset-password.ts
@@ -0,0 +1,43 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Reset Password', () => {
+  it('should show error if password does not match', () => {
+    cy.visit('/reset-password?code=123456789')
+
+    cy.findAllByPlaceholderText(/^password/i).type('123')
+    cy.findAllByPlaceholderText(/confirm password/i).type('1234')
+    cy.findByRole('button', { name: /reset password/i }).click()
+
+    cy.findByText(/confirm password does not match with password/i).should(
+      'exist'
+    )
+  })
+
+  it('should show error with code is not valid', () => {
+    cy.intercept('POST', '**/auth/reset-password', (res) => {
+      res.reply({
+        status: 400,
+        body: {
+          error: 'Bad request',
+          message: [
+            {
+              messages: [
+                {
+                  message: 'Incorrect code provided'
+                }
+              ]
+            }
+          ]
+        }
+      })
+    })
+
+    cy.visit('/reset-password?code=wrong_code')
+
+    cy.findAllByPlaceholderText(/^password/i).type('123')
+    cy.findAllByPlaceholderText(/confirm password/i).type('123')
+    cy.findByRole('button', { name: /reset password/i }).click()
+
+    cy.findByText(/Incorrect code provided/i).should('exist')
+  })
+})
